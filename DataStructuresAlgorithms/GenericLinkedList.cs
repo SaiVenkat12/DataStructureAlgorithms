730ffb21250cd388b30c664b0b3f54d308a32e2d
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-01 19:25:42 +0530

    [SaiVenkat]-Create: Created an Unordered List

---
+++ DataStructuresAlgorithms/GenericLinkedList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAlgorithms
+{
+    public class GenericLinkedList<G>
+    {
+            public Node<G> head;
+
+            internal void Add(G data)
+            {
+                Node<G> node = new Node<G>(data);
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    Node<G> temp = head;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                Console.WriteLine(node.data);
+            }
+            public void Search(G word)
+            {
+                Node<G> temp = this.head;
+                while (temp != null)
+                {
+                    if (temp.data.Equals(word))
+                    {
+                        Console.WriteLine("This Word: {0} is found ", temp.data);
+
+                        return;
+                    }
+                    temp = temp.next;
+                }
+                if (temp == null)
+                {
+                    Console.WriteLine("This Word: {0} is not found", word);
+                    Add(word);
+                }
+            }
+
+            internal void Display()
+            {
+                Node<G> temp = this.head;
+                if (temp == null)
+                {
+                    Console.WriteLine("Linked list is empty");
+                }
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+    }
+}
---
+++ DataStructuresAlgorithms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DataStructuresAlgorithms;
+
+namespace DataStructureAlgorithms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GenericLinkedList<string> words = new GenericLinkedList<string>();
+            string sentence = "If you look at what you don't have in life, you'll never have enough";
+            string[] arr = sentence.Split(" ");
+
+            foreach (string data in arr)
+            {
+                words.Add(data);
+            }
+            Console.WriteLine("Search the word : ");
+            string word = Console.ReadLine();
+            words.Search(word);
+            words.Display();
+        }
+    }
+}
